56a64f2584d9918e207733c8e3eaa13295d9d7a8
Anthony Iacano <iacano5@outlook.com>
2023-09-11 17:57:42 -0400

    adjusted circle test

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -2,10 +2,9 @@ const Circle = require('../lib/circle.js');
 
 
 describe('Circle', () => {
-    // TODO: Write a test that checks if isPassword() returns false when the password is less than 8 characters.
-  
+
     describe('Renders Circle', () => {
-      it('should return false for password less than 8 characters long', () => {
+      it('gives us back the text for a blue circle', () => {
         const shape = new Circle();
         shape.setColor("blue");
         expect(shape.render()).toEqual("<circle cx='150px' cy='100px' r='75' fill='blue' />");
